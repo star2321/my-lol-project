a25f217a591368a202d137e7ecbb15472d9195ad
hyunjoon <joon0608@naver.com>
2017-04-06 09:58:43 +0900

    Changed the HTML for the site.

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 
 
 class Post(models.Model):
-    summoner = models.ForeignKey('self')
+    summoner = models.ForeignKey('auth.User')
     id_key = models.CharField(max_length=200)
 
     def search(self):
